77c45fbc5a091574aa36996969399332074899f3
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-18 21:36:24 -0600

    added error handling and console.logs to figure out the errors

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -130,6 +130,11 @@ const handleRenderBtns = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
+  if (!jsonNotes || !Array.isArray(jsonNotes)) {
+    // Handle unexpected data format
+    console.error('Unexpected data format:', jsonNotes);
+    return;
+  }
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
