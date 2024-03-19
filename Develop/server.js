6c51b0f0cda6348e015975344c8de4db250c9b1d
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-18 21:35:17 -0600

    added better readability

--- Develop/server.js
+++ Develop/server.js
@@ -3,16 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const { v4: uuidv4 } = require('uuid');
 
-// Middleware setup
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Import routes with corrected paths
 const apiRoutes = require('./db/routes/apiRoutes');
 const htmlRoutes = require('./db/routes/htmlRoutes');
 
-// Use routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
